8ed361d0585d59f1b4ce338180f27565558e4e76
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-06-03 15:05:18 -0300

    First commit

---
+++ src/main/java/com/matheuscordeiro/marvelapiasyncservices/MarvelApiAsyncServicesApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.marvelapiasyncservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelApiAsyncServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelApiAsyncServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/marvelapiasyncservices/MarvelApiAsyncServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.marvelapiasyncservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelApiAsyncServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
